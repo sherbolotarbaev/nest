1c84b3b4de5145c470dbd966117634ea6175e818
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-01-13 02:30:35 +0600

    Update

--- src/main.ts
+++ src/main.ts
@@ -23,4 +23,5 @@ async function start() {
   await app.listen(process.env.PORT);
   logger.log(`Server is running on: http://localhost:${process.env.PORT} ⚡️`);
 }
-export default start();
+
+start();
