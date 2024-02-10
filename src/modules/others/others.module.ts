b35446d3f7d088944691352408c0f16be17c33f3
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-02-11 02:41:50 +0600

    Update

--- src/modules/others/others.module.ts
+++ src/modules/others/others.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { OthersService } from './others.service';
 import { OthersController } from './others.controller';
-import { UsersService } from '@users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
   providers: [OthersService, UsersService],
