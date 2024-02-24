421085f918d00393fca296de3aebe74ff6c23946
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-02-25 04:38:06 +0600

    fix: added for chat module UsersService

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { UsersService } from '@users/users.service';
 
 @Module({
-  providers: [ChatService],
+  providers: [ChatService, UsersService],
   controllers: [ChatController]
 })
 export class ChatModule {}
